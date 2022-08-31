be9915e3008b8808fd302472c0310ad664fb003c
Fernando A <dellama.f@gmail.com>
2022-08-31 16:09:44 -0300

    exerc 1-5

---
+++ Fernando A/Exercícios_Python/01 Estrutura_Sequencial/exerc4.py
@@ -0,0 +1,6 @@
+n1=float(input("Digite a nota do 1o bimestre:"))
+n2=float(input('Digite a nota do 2o bimestre:'))
+n3=float(input('Digite a nota do 3o bimestre:'))
+n4=float(input('Digite a nota do 4o bimestre:'))
+media=(n1+n2+n3+n4)/4
+print('A sua média é:',media)
---
+++ Fernando A/Exercícios_Python/01 Estrutura_Sequencial/exerc5.py
@@ -0,0 +1,3 @@
+metros = int(input('Informe o valor em metros: '))
+
+print ('O valor em centímetros é:', metros * 100)
